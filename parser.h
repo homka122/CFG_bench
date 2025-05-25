533f22c4c54aa89bd48e3b694767773d892c6b61
Homka122 <kombaev02@gmail.com>
2025-05-25 17:39:20 +0300

    Feat: add to header file symbol_list methods

--- parser.h
+++ parser.h
@@ -78,3 +78,14 @@ typedef struct {
 } ParserResult;
 
 ParserResult parser(char *config_i);
+
+// SymbolList ans Symbol methods
+Symbol symbol_create(char *str);
+void symbol_free(Symbol sym);
+SymbolList symbol_list_create();
+void symbol_list_print(SymbolList list);
+int symbol_list_add_str(SymbolList *list, char *str, bool is_nonterm);
+int symbol_list_add_str_start_nonterm(SymbolList *list, char *str);
+void symbol_list_free(SymbolList *list);
+bool is_non_terminal(const Symbol symbol, SymbolList non_terms);
+void grammar_print(Grammar grammar, SymbolList list);
