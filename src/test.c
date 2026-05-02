f48ecb1109f142fe092966ce792d971bd173af1b
homka122 <kombaev02@gmail.com>
2026-05-02 21:00:16 +0300

    Fix: fix grammar error in info message

--- src/test.c
+++ src/test.c
@@ -136,7 +136,7 @@ int main(int argc, char **argv) {
     bool is_hot_enabled = false;
     bool is_config = false;
     char *algo = NULL;
-    bool is_algo_choosed = false;
+    bool is_algo_chosen = false;
     char *input_config = NULL;
     size_t rounds_count = 10;
 
@@ -181,7 +181,7 @@ int main(int argc, char **argv) {
             printf("Choosen config: %s\n", input_config);
             break;
         case 'a':
-            is_algo_choosed = true;
+            is_algo_chosen = true;
             algo = optarg;
             printf("Choosen algorithm: %s\n", algo);
 
@@ -208,10 +208,10 @@ int main(int argc, char **argv) {
         }
     }
 
-    if (!is_algo_choosed) {
+    if (!is_algo_chosen) {
         adapter = adapter_CFL_adv_get_methods();
         algo = "CFL_adv";
-        printf("No algorithm choosed, using CFL_adv by default\n");
+        printf("No algorithm chosen, using CFL_adv by default\n");
     }
 
     TRY(adapter.setup());
